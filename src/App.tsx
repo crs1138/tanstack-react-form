ecde3707d7b56ee5b6a78718e6fe94eb256c003c
crs1138 <1313681+crs1138@users.noreply.github.com>
2025-11-28 09:18:10 +0100

    Feat(Field): Create basic form using @tanstack-react-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,142 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { formOptions, useForm } from '@tanstack/react-form'
 import './App.css'
 
+interface User {
+  firstName: string
+  lastName: string
+  email: string
+  hobbies: string[]
+}
+
+const defaultUser: User = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  hobbies: [],
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const formOpts = formOptions({
+    defaultValues: defaultUser,
+  })
+  const form = useForm({
+    ...formOpts,
+    onSubmit: async ({ value }) => {
+      alert(value.hobbies.join(', '))
+      console.log(value)
+    },
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      <form>
+        <h1>User Form</h1>
+        <div>
+          <form.Field
+            name="firstName"
+            children={(field) => (
+              <>
+                <label htmlFor="firstName">First Name</label>
+                <input
+                  id="firstName"
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  onBlur={field.handleBlur}
+                />
+              </>
+            )}
+          />
+        </div>
+        <div>
+          <form.Field
+            name="lastName"
+            children={(field) => (
+              <>
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                  id="lastName"
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  onBlur={field.handleBlur}
+                />
+              </>
+            )}
+          />
+        </div>
+        <div>
+          <form.Field
+            name="email"
+            children={(field) => (
+              <>
+                <label htmlFor="email">Email</label>
+                <input
+                  id="email"
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  onBlur={field.handleBlur}
+                />
+              </>
+            )}
+          />
+        </div>
+        <div>
+          <form.Field
+            name="hobbies"
+            children={(field) => (
+              <>
+                <fieldset>
+                  <legend>Hobbies</legend>
+                  <input
+                    id="reading"
+                    name="hobbies"
+                    type="checkbox"
+                    value="reading"
+                    onChange={(e) => field.handleChange([...field.state.value, e.target.value])}
+                    onBlur={field.handleBlur}
+                  />
+                  <label htmlFor="reading">Reading</label>
+                  <input
+                    id="writing"
+                    name="hobbies"
+                    type="checkbox"
+                    value="writing"
+                    onChange={(e) => field.handleChange([...field.state.value, e.target.value])}
+                    onBlur={field.handleBlur}
+                  />
+                  <label htmlFor="writing">Writing</label>
+                  <input
+                    id="coding"
+                    name="hobbies"
+                    type="checkbox"
+                    value="coding"
+                    onChange={(e) => field.handleChange([...field.state.value, e.target.value])}
+                    onBlur={field.handleBlur}
+                  />
+                  <label htmlFor="coding">Coding</label>
+                  <input
+                    id="other"
+                    name="hobbies"
+                    type="checkbox"
+                    value="other"
+                    onChange={(e) => field.handleChange([...field.state.value, e.target.value])}
+                    onBlur={field.handleBlur}
+                  />
+                  <label htmlFor="other">Other</label>
+                </fieldset>
+              </>
+            )}
+          />
+        </div>
+        <button
+          type="submit"
+          onClick={(e) => {
+            form.handleSubmit()
+          }}
+        >
+          {form.state.isSubmitting ? 'Submitting...' : 'Submit'}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </form>
+    </div>
   )
 }
 
